b3fb181495d192d9e94a26c0988226d878a547eb
Ian Woodard <17186604+IanWoodard@users.noreply.github.com>
2023-11-29 16:21:16 -0800

    test: manual_tests.scenarios.feature_branch_happy_path (2023-11-29 16:18:24.505220)

---
+++ terraform/env/prod/slice-1-iam/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2023-11-30 00:21:16.895197+00:00"
+  }
+}
---
+++ terraform/env/prod/slice-2-vm/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2023-11-30 00:21:16.939676+00:00"
+  }
+}
