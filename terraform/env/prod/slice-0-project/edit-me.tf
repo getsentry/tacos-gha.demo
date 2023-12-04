431f33bdbc289f09d752d88d1176e970873aa228
Ian Woodard <17186604+IanWoodard@users.noreply.github.com>
2023-12-04 11:56:30 -0800

    test: manual_tests.behaviors.apply_on_update (2023-12-04 11:56:28.831361)

---
+++ terraform/env/prod/slice-0-project/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2023-12-04 19:56:30.808425+00:00"
+  }
+}
---
+++ terraform/env/prod/slice-2-vm/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2023-12-04 19:56:30.826563+00:00"
+  }
+}
