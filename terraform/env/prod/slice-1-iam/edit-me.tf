d81ce844c1572f63848a2df8ca0cac2e1e005893
Buck Evan <buck.evan@sentry.io>
2023-12-05 10:49:12 -0600

    test: manual_tests.behaviors.lock_on_pr (2023-12-05 10:49:11.285810)

---
+++ terraform/env/prod/slice-1-iam/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2023-12-05 16:49:12.557754+00:00"
+  }
+}
---
+++ terraform/env/prod/slice-2-vm/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2023-12-05 16:49:12.573456+00:00"
+  }
+}
