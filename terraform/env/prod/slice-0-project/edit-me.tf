45ed5cfe4506d22e93b20dcbb789dc91cad7a735
Ellison Marks <ellison.marks@sentry.io>
2023-12-05 14:40:51 -0800

    test: manual_tests.behaviors.apply_on_update (2023-12-05 14:40:49.736827)

---
+++ terraform/env/prod/slice-0-project/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2023-12-05 22:40:51.852061+00:00"
+  }
+}
---
+++ terraform/env/prod/slice-1-iam/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2023-12-05 22:40:51.861169+00:00"
+  }
+}
