8b7c8122b99362c30d15200c0bbbb478cb65cdc4
Buck Evan <buck.evan@sentry.io>
2023-12-06 16:44:33 -0600

    test: manual_tests.behaviors.apply_on_update (2023-12-06 16:44:32.228771)

---
+++ terraform/env/prod/slice-0-project/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2023-12-06 22:44:33.441682+00:00"
+  }
+}
---
+++ terraform/env/prod/slice-1-iam/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2023-12-06 22:44:33.451730+00:00"
+  }
+}
