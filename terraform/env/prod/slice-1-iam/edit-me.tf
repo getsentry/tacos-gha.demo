34460b8e2c174c976728dd724c016258772c8a8c
Buck Evan <buck.evan@sentry.io>
2023-12-06 13:26:30 -0600

    test: feature_branch_happy_path (2023-12-06 13:26:29.018749)

---
+++ terraform/env/prod/slice-1-iam/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2023-12-06 19:26:30.234076+00:00"
+  }
+}
---
+++ terraform/env/prod/slice-2-vm/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2023-12-06 19:26:30.243745+00:00"
+  }
+}
