8612387482e0fb57bc6463445617026c1b5a3a5f
Ellison Marks <ellison.marks@sentry.io>
2023-12-07 14:46:37 -0800

    test: manual_tests.behaviors.unlock_on_merge (2023-12-07 14:46:34.648907)

---
+++ terraform/env/prod/slice-1-iam/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2023-12-07 22:46:37.532584+00:00"
+  }
+}
---
+++ terraform/env/prod/slice-2-vm/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2023-12-07 22:46:37.542215+00:00"
+  }
+}
