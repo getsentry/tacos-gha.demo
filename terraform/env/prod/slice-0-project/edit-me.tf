c516f112184c832f1a769160437838e4a2b41b90
Buck Evan <buck.evan@sentry.io>
2023-12-07 14:49:59 -0600

    test: message_on_lock_failure (2023-12-07 14:45:35.086472)

---
+++ terraform/env/prod/slice-0-project/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2023-12-07 20:49:59.186921+00:00"
+  }
+}
---
+++ terraform/env/prod/slice-1-iam/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2023-12-07 20:49:59.198381+00:00"
+  }
+}
