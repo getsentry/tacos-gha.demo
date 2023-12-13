434fc3da3b8731dc7a1cb701a0ee75856ce4f1c3
Ian Woodard <17186604+IanWoodard@users.noreply.github.com>
2023-12-13 15:32:11 -0800

    test: manual_tests.scenarios.draft_pr (2023-12-13 15:32:09.405063)

---
+++ terraform/env/prod/slice-0-project/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2023-12-13 23:32:11.377837+00:00"
+  }
+}
---
+++ terraform/env/prod/slice-1-iam/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2023-12-13 23:32:11.399123+00:00"
+  }
+}
