886848012628c2d7f59226075444b43ba3ce3513
Buck Evan <buck.evan@sentry.io>
2023-12-15 14:04:13 -0600

    test: force_unlock_on_dirty (2023-12-15 20:01:53.101495+00:00)

---
+++ terraform/env.buck/prod/slice-1-iam/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2023-12-15 20:04:13.783212+00:00"
+  }
+}
---
+++ terraform/env.buck/prod/slice-2-vm/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2023-12-15 20:04:13.772539+00:00"
+  }
+}
