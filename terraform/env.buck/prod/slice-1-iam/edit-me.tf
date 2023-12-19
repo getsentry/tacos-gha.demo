ac50ac6d47b6759a3ecb92234e66cedcf2e8769b
Buck Evan <buck.evan@sentry.io>
2023-12-18 18:25:35 -0600

    test: unlock_on_merge (2023-12-19 00:19:57.481927+00:00)

---
+++ terraform/env.buck/prod/slice-1-iam/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2023-12-19 00:25:35.632409+00:00"
+  }
+}
---
+++ terraform/env.buck/prod/slice-2-vm/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2023-12-19 00:25:35.619665+00:00"
+  }
+}
