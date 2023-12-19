88eaa77e2fabdb3c59b6be7acb6ee8757365cbd0
Buck Evan <buck.evan@sentry.io>
2023-12-18 18:10:41 -0600

    test: separate_changes_dont_conflict (2023-12-18 23:59:12.534611+00:00)

---
+++ terraform/env.buck/prod/slice-0-project/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2023-12-19 00:10:41.793913+00:00"
+  }
+}
---
+++ terraform/env.buck/prod/slice-2-vm/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2023-12-19 00:10:41.781518+00:00"
+  }
+}
