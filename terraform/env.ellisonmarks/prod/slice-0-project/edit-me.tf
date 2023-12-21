6d1648ff763aee5dee0b8592c850bba305baaf3e
Ellison Marks <ellison.marks@sentry.io>
2023-12-21 01:14:54 -0800

    test: plan_on_push (2023-12-21 09:14:51.572379+00:00)

---
+++ terraform/env.ellisonmarks/prod/slice-0-project/edit-me.tf
@@ -0,0 +1,6 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now   = "2023-12-21 09:14:54.064683+00:00"
+    slice = "/private/var/folders/60/4wmvhbl95vsg3dlt5f37czwm0000gn/T/pytest-of-ellisonmarks/pytest-2/test0/tacos-demo/terraform/env.ellisonmarks/prod/slice-0-project"
+  }
+}
