04bc1ed20f4fc25fe79caeed8c0ac74aaa58d8e7
Buck Evan <buck.evan@sentry.io>
2024-01-06 13:44:57 -0600

    test: force_unlock_on_happy_path (2024-01-06 19:37:20.345841+00:00)

---
+++ terraform/env.buck/prod/slice-0-project/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-01-06 19:44:57.143286+00:00"
+  }
+}
---
+++ terraform/env.buck/prod/slice-2-vm/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-01-06 19:44:57.132532+00:00"
+  }
+}
