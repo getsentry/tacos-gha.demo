43e8391a1535705eeeae635e4fb132483c9773df
Ellison Marks <ellison.marks@sentry.io>
2024-01-08 15:25:18 -0800

    test: unlock_on_merge (2024-01-08 23:25:14.494158+00:00)

---
+++ terraform/env.ellisonmarks/prod/slice-1-iam/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-01-08 23:25:18.952268+00:00"
+  }
+}
---
+++ terraform/env.ellisonmarks/prod/slice-2-vm/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-01-08 23:25:18.944730+00:00"
+  }
+}
