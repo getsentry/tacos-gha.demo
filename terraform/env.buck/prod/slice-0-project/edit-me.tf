29ec4145312f1f1402eb6f295db58f976e162f31
Buck Evan <112430378+bukzor-sentryio@users.noreply.github.com>
2024-01-09 10:10:00 -0600

    test: unlock_on_merge (2024-01-09 16:08:31.265321+00:00) (#644)

---
+++ terraform/env.buck/prod/slice-0-project/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-01-09 16:08:40.014896+00:00"
+  }
+}
---
+++ terraform/env.buck/prod/slice-2-vm/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-01-09 16:08:39.997464+00:00"
+  }
+}
