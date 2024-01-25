adf7d65629ad6807e775935fee16a08c580efa51
Buck Evan <buck.evan@sentry.io>
2024-01-25 16:37:43 -0600

    test: behaviors-unlock_on_merge (2024-01-25 22:37:15.653021+00:00)

---
+++ terraform/env.buck/behaviors-unlock_on_merge/slice-1-iam/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-01-25 22:37:43.039775+00:00"
+  }
+}
---
+++ terraform/env.buck/behaviors-unlock_on_merge/slice-2-vm/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-01-25 22:37:43.029770+00:00"
+  }
+}
