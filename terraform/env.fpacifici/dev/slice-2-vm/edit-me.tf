dd58c92b21ec8dcdbe4e2f4541a5e00640bcf25a
Filippo Pacifici <fpacifici@sentry.io>
2024-01-24 19:14:07 -0800

    test: apply_on_update (2024-01-25 03:13:46.948651+00:00)

---
+++ terraform/env.fpacifici/dev/slice-2-vm/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-01-25 03:14:07.569343+00:00"
+  }
+}
---
+++ terraform/env.fpacifici/prod/slice-0-project/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-01-25 03:14:07.576980+00:00"
+  }
+}
