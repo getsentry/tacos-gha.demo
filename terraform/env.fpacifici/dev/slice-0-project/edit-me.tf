67885d2f27d636d9d8ed559eae1a9fe8bd94f86a
Filippo Pacifici <fpacifici@sentry.io>
2024-01-25 08:30:26 -0800

    test: abandon_a_branch (2024-01-25 16:15:41.471498+00:00)

---
+++ terraform/env.fpacifici/dev/slice-0-project/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-01-25 16:30:26.492323+00:00"
+  }
+}
---
+++ terraform/env.fpacifici/prod/slice-1-iam/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-01-25 16:30:26.498849+00:00"
+  }
+}
