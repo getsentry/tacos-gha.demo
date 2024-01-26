9a68ceed552f9087e046c3759bc3733bc1c4e138
Filippo Pacifici <fpacifici@sentry.io>
2024-01-26 15:51:26 -0800

    test: force_unlock_on_happy_path (2024-01-26 23:51:08.647791+00:00)

---
+++ terraform/env.fpacifici/dev/slice-2-vm/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-01-26 23:51:26.494781+00:00"
+  }
+}
---
+++ terraform/env.fpacifici/prod/slice-2-vm/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-01-26 23:51:26.488059+00:00"
+  }
+}
