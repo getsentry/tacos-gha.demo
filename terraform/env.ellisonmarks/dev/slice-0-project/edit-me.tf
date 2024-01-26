59ba3772ecc760d01aff8073fe02d4aa70b4100c
ellisonmarks <125329741+ellisonmarks@users.noreply.github.com>
2024-01-26 00:11:07 -0800

    test: draft_pr (2024-01-26 08:06:57.269207+00:00) (#992)
    * test: draft_pr (2024-01-26 08:06:57.269207+00:00)
    
    * test: draft_pr-2 (2024-01-26 08:06:57.269207+00:00)

---
+++ terraform/env.ellisonmarks/dev/slice-0-project/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-01-26 08:08:32.812395+00:00"
+  }
+}
---
+++ terraform/env.ellisonmarks/dev/slice-1-iam/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-01-26 08:08:32.820380+00:00"
+  }
+}
