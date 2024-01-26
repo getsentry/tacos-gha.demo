2aca3b11724ef8f32fbc177784afaf5b5013c2c2
Filippo Pacifici <fpacifici@sentry.io>
2024-01-25 17:08:21 -0800

    test: lock_contention (2024-01-26 00:54:35.110899+00:00)

---
+++ terraform/env.fpacifici/prod/slice-0-project/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-01-26 01:08:21.311085+00:00"
+  }
+}
---
+++ terraform/env.fpacifici/prod/slice-2-vm/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-01-26 01:08:21.295777+00:00"
+  }
+}
