ea6fbab2a7c512dea1bf596ab3254998fa12fd1a
Buck Evan <buck.evan@sentry.io>
2024-01-29 10:31:08 -0600

    test: behaviors-force_unlock_on_dirty (2024-01-29 16:29:43.115213+00:00)

---
+++ terraform/env.buck/behaviors-force_unlock_on_dirty/slice-1-iam/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-01-29 16:31:08.592327+00:00"
+  }
+}
---
+++ terraform/env.buck/behaviors-force_unlock_on_dirty/slice-2-vm/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-01-29 16:31:08.600837+00:00"
+  }
+}
