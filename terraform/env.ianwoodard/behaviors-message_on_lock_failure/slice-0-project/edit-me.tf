db80877ad91f0aa47b4b008ea453be33f819c56b
Ian Woodard <17186604+IanWoodard@users.noreply.github.com>
2024-01-29 10:22:46 -0800

    test: behaviors-message_on_lock_failure (2024-01-29 18:21:16.667897+00:00)

---
+++ terraform/env.ianwoodard/behaviors-message_on_lock_failure/slice-0-project/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-01-29 18:22:46.100253+00:00"
+  }
+}
---
+++ terraform/env.ianwoodard/behaviors-message_on_lock_failure/slice-2-vm/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-01-29 18:22:46.122578+00:00"
+  }
+}
