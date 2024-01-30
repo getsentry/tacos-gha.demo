1d02ae1345ac29a16abd18a6ca3888d275aeee39
Filippo Pacifici <fpacifici@sentry.io>
2024-01-29 18:35:18 -0800

    test: scenarios-draft_pr (2024-01-30 02:34:22.325416+00:00)

---
+++ terraform/env.fpacifici/prod/slice-1-iam/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-01-30 02:35:18.241428+00:00"
+  }
+}
---
+++ terraform/env.fpacifici/prod/slice-2-vm/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-01-30 02:35:18.212184+00:00"
+  }
+}
