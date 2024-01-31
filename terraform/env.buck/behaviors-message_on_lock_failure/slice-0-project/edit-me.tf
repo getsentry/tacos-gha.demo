760d595b924d543bd8764a52fb2cdceb30e9e2eb
Buck Evan <buck.evan@sentry.io>
2024-01-31 00:09:25 -0600

    test: behaviors-message_on_lock_failure (2024-01-31 06:08:32.599524+00:00)

---
+++ terraform/env.buck/behaviors-message_on_lock_failure/slice-0-project/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-01-31 06:09:25.611529+00:00"
+  }
+}
---
+++ terraform/env.buck/behaviors-message_on_lock_failure/slice-2-vm/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-01-31 06:09:25.621040+00:00"
+  }
+}
