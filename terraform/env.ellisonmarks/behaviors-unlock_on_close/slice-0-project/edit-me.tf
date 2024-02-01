0374e536c8c763b4c6035312e6d8b04b66237306
Ellison Marks <ellison.marks@sentry.io>
2024-02-01 15:29:41 -0800

    test: behaviors-unlock_on_close (2024-02-01 23:29:24.317291+00:00)

---
+++ terraform/env.ellisonmarks/behaviors-unlock_on_close/slice-0-project/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-02-01 23:29:41.661426+00:00"
+  }
+}
---
+++ terraform/env.ellisonmarks/behaviors-unlock_on_close/slice-1-iam/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-02-01 23:29:41.670070+00:00"
+  }
+}
