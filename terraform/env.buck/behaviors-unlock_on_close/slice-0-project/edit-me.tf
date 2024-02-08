87bd9be063d834f1480e3e3f4261d19866ec2cb0
Buck Evan <buck.evan@sentry.io>
2024-02-07 19:21:26 -0600

    test: behaviors-unlock_on_close (2024-02-08 01:19:45.591270+00:00)

---
+++ terraform/env.buck/behaviors-unlock_on_close/slice-0-project/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-02-08 01:21:26.593636+00:00"
+  }
+}
---
+++ terraform/env.buck/behaviors-unlock_on_close/slice-1-iam/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-02-08 01:21:26.603510+00:00"
+  }
+}
