8abc1b3d95238f13b3516ee1b1360b02e572ee2e
Ellison Marks <ellison.marks@sentry.io>
2024-02-07 16:27:26 -0800

    test: scenarios-lock_contention (2024-02-08 00:26:10.069359+00:00)

---
+++ terraform/env.ellisonmarks/scenarios-lock_contention/slice-1-iam/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-02-08 00:27:26.923749+00:00"
+  }
+}
---
+++ terraform/env.ellisonmarks/scenarios-lock_contention/slice-2-vm/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-02-08 00:27:26.914284+00:00"
+  }
+}
