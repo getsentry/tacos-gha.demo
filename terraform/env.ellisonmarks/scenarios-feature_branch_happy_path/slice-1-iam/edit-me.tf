3e6e176667a0e69d427a7626bb3fc3b740032374
ellisonmarks <125329741+ellisonmarks@users.noreply.github.com>
2024-02-08 12:04:53 -0800

    test: scenarios-feature_branch_happy_path (2024-02-08 19:57:48.181959+00:00) (#1565)
    * test: scenarios-feature_branch_happy_path (2024-02-08 19:57:48.181959+00:00)
    
    * more changes

---
+++ terraform/env.ellisonmarks/scenarios-feature_branch_happy_path/slice-1-iam/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-02-08 20:03:25.497032+00:00"
+  }
+}
---
+++ terraform/env.ellisonmarks/scenarios-feature_branch_happy_path/slice-2-vm/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-02-08 20:03:25.487082+00:00"
+  }
+}
