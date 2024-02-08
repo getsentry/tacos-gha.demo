e7d9b265dbf9b07cf3187b21aee05ac66b2fcb25
Buck Evan <buck.evan@sentry.io>
2024-02-08 02:04:14 -0600

    test: scenarios-lock_contention (2024-02-08 08:03:47.561657+00:00)

---
+++ terraform/env.buck/scenarios-lock_contention/slice-0-project/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-02-08 08:04:14.970572+00:00"
+  }
+}
---
+++ terraform/env.buck/scenarios-lock_contention/slice-1-iam/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-02-08 08:04:14.960813+00:00"
+  }
+}
