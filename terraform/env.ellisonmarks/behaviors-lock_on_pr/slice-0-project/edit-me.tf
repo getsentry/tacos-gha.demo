e48a738b402ebe6d5cbfce0f94846c71805ba81d
Ellison Marks <ellison.marks@sentry.io>
2024-02-07 16:28:31 -0800

    test: behaviors-lock_on_pr (2024-02-08 00:26:08.966028+00:00)

---
+++ terraform/env.ellisonmarks/behaviors-lock_on_pr/slice-0-project/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-02-08 00:28:31.675534+00:00"
+  }
+}
---
+++ terraform/env.ellisonmarks/behaviors-lock_on_pr/slice-2-vm/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-02-08 00:28:31.666734+00:00"
+  }
+}
