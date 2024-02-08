57af4b2b4372cd4297e60d8854af8c4847f31ebc
Buck Evan <buck.evan@sentry.io>
2024-02-08 12:31:59 -0600

    test: scenarios-abandon_a_branch (2024-02-08 18:31:18.918258+00:00)

---
+++ terraform/env.buck/scenarios-abandon_a_branch/slice-1-iam/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-02-08 18:31:58.898290+00:00"
+  }
+}
---
+++ terraform/env.buck/scenarios-abandon_a_branch/slice-2-vm/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-02-08 18:31:58.884406+00:00"
+  }
+}
