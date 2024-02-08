11cf946c25b002781bc003b84dd3c0e4f779b904
Ellison Marks <ellison.marks@sentry.io>
2024-02-07 16:27:59 -0800

    test: behaviors-force_unlock_on_dirty (2024-02-08 00:26:09.034002+00:00)

---
+++ terraform/env.ellisonmarks/behaviors-force_unlock_on_dirty/slice-0-project/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-02-08 00:27:59.236935+00:00"
+  }
+}
---
+++ terraform/env.ellisonmarks/behaviors-force_unlock_on_dirty/slice-1-iam/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-02-08 00:27:59.245431+00:00"
+  }
+}
