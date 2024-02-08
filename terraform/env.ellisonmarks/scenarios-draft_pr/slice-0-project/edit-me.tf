e99c1dc4386714c20c1f179e97de1c57f620f047
ellisonmarks <125329741+ellisonmarks@users.noreply.github.com>
2024-02-07 16:50:48 -0800

    test: scenarios-draft_pr (2024-02-08 00:45:10.811217+00:00) (#1353)
    * test: scenarios-draft_pr (2024-02-08 00:45:10.811217+00:00)
    
    * test: scenarios-draft_pr-2 (2024-02-08 00:45:10.811217+00:00)

---
+++ terraform/env.ellisonmarks/scenarios-draft_pr/slice-0-project/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-02-08 00:47:38.871742+00:00"
+  }
+}
---
+++ terraform/env.ellisonmarks/scenarios-draft_pr/slice-1-iam/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-02-08 00:47:38.879931+00:00"
+  }
+}
