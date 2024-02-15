4b2e9ddcea475f2b228b0c171b73a96f6cd0b3c0
Buck Evan <buck.evan@sentry.io>
2024-02-15 17:13:35 -0600

    test: behaviors-plan_on_push (2024-02-15 23:12:54.019549+00:00)

---
+++ terraform/env.buck/behaviors-plan_on_push/slice-0-project/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-02-15 23:13:35.882177+00:00"
+  }
+}
---
+++ terraform/env.buck/behaviors-plan_on_push/slice-1-iam/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-02-15 23:13:35.863345+00:00"
+  }
+}
