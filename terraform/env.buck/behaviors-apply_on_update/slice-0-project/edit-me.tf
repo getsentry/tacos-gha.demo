678581e914e48483ecec426d5e3403c3ceee4cb1
TACOS-gha Tester <nobody+tacos-gha@sentry.io>
2024-02-19 16:31:12 -0600

    test: behaviors-apply_on_update (2024-02-19 22:30:36.196381+00:00)

---
+++ terraform/env.buck/behaviors-apply_on_update/slice-0-project/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-02-19 22:31:12.854712+00:00"
+  }
+}
---
+++ terraform/env.buck/behaviors-apply_on_update/slice-2-vm/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-02-19 22:31:12.845764+00:00"
+  }
+}
