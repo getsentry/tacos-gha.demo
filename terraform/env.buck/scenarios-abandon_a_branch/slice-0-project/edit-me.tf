a50eb17bea12af076d60ffaa8d9a2343b25fc71f
TACOS-gha Tester <nobody+tacos-gha@sentry.io>
2024-02-26 09:16:56 -0600

    test: scenarios-abandon_a_branch (2024-02-26 15:14:56.759952+00:00)

---
+++ terraform/env.buck/scenarios-abandon_a_branch/slice-0-project/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-02-26 15:16:56.548924+00:00"
+  }
+}
---
+++ terraform/env.buck/scenarios-abandon_a_branch/slice-1-iam/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-02-26 15:16:56.537090+00:00"
+  }
+}
