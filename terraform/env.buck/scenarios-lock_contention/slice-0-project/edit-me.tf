038f86f1981ca4cf7e0ccf4a522d51c19ee3e067
TACOS-gha Tester <nobody+tacos-gha@sentry.io>
2024-03-06 15:55:39 -0600

    test: scenarios-lock_contention (2024-03-06 21:55:10.743563+00:00)

---
+++ terraform/env.buck/scenarios-lock_contention/slice-0-project/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-03-06 21:55:39.928781+00:00"
+  }
+}
---
+++ terraform/env.buck/scenarios-lock_contention/slice-1-iam/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-03-06 21:55:39.939258+00:00"
+  }
+}
