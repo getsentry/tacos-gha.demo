5a610a3d56c4ead3ea9aec26698d2bf195d66198
TACOS-gha Tester <nobody+tacos-gha@sentry.io>
2024-05-23 16:28:15 -0700

    test: behaviors-unlock_on_close (2024-05-23 23:27:42.305262+00:00)

---
+++ terraform/env.neohuang/behaviors-unlock_on_close/slice-1-iam/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-05-23 23:28:15.033484+00:00"
+  }
+}
---
+++ terraform/env.neohuang/behaviors-unlock_on_close/slice-2-vm/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-05-23 23:28:15.057230+00:00"
+  }
+}
