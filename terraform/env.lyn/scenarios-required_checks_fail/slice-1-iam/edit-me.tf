46267a6aed161b29a2309cd4c74b516e38a7fc7b
TACOS-gha Tester <nobody+tacos-gha@sentry.io>
2024-07-24 16:23:34 -0700

    test: scenarios-required_checks_fail (2024-07-24 23:23:03.661862+00:00)

---
+++ terraform/env.lyn/scenarios-required_checks_fail/slice-1-iam/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-07-24 23:23:34.082783+00:00"
+  }
+}
---
+++ terraform/env.lyn/scenarios-required_checks_fail/slice-2-vm/edit-me.tf
@@ -0,0 +1,5 @@
+resource "null_resource" "edit-me" {
+  triggers = {
+    now = "2024-07-24 23:23:34.101406+00:00"
+  }
+}
